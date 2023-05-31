f6462e5829ee2db3b649b499a42a9e148b4bbb8b
65-74197\USER <Tatsuya_Aratani@totec.co.jp>
2023-05-31 14:56:19 +0900

    7-1 p426

--- photoproject/photo/models.py
+++ photoproject/photo/models.py
@@ -40,5 +40,35 @@ class photoPost(models.Model):
        on_delete=models.PROTECT
     )
 #タイトル用のフィールド
+title = models.CharField(
+   verbose_name='タイトル',#フィールドのタイトル
+   max_length=200         #最大文字数は200
+)
+#コメント用のフィールド
+comment = models.TextField(
+   verbose_name='コメント',#フィールドのタイトル
+)
+#イメージのフィールド1
+image1 = models.ImageField(
+   verbose_name='イメージ1',#フィールドのタイトル
+   upload_to = 'photos'    #MEDIA_ROOT以下のphotosにファイルを保存
+)
+#イメージのフィールド2
+image2 = models.ImageField(
+   verbose_name='イメージ2',#フィールドのタイトル
+   upload_to = 'photos',    #MEDIA_ROOT以下のphotosにファイルを保存
+   blank=True,             #フィールド値の設定は必須ではない
+   null=True               #データベースにnullが保存されることを許容
+)
+#投稿日時のフィールド
+posted_at = models.DateTimeField(
+   verbose_name='投稿日時',#フィールドのタイトル
+   auto_now_add=True      #日時を自動追加
+)
 
+def __str___(self):
+   '''オブジェクトを文字列に変換して返す
+   
+   Returns(str):投稿記事のタイトル'''
+   return self.title
 # Create your models here.
